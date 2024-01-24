9ea48c13f50338bd1d69d89f5972a2da2b1a9e7d
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-01-24 21:46:00 +0530

    Homepage UI - Final part & Profile page UI

--- src/components/profileCard/ProfileCard.js
+++ src/components/profileCard/ProfileCard.js
@@ -4,6 +4,9 @@ import Cover from '../../img/cover.jpg'
 import Profile from '../../img/profileImg.jpg'
 
 const ProfileCard = () => {
+
+    const ProfilePage = true
+
     return (
         <div className='ProfileCard'>
             <div className="profile-images">
@@ -26,10 +29,19 @@ const ProfileCard = () => {
                         <span>150</span>
                         <span>Following</span>
                     </div>
+                    {ProfilePage &&
+                        <>
+                            <div className="vl"></div>
+                            <div className="follow">
+                                <span>3</span>
+                                <span>Posts</span>
+                            </div>
+                        </>
+                    }
                 </div>
                 <hr />
             </div>
-            <span>My Profile</span>
+            {ProfilePage ? "" : <span>My Profile</span>}
         </div>
     )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './Home.css'
 import ProfileSide from '../../components/profileSide/ProfileSide'
 import PostSide from '../../components/postSide/PostSide'
+import RightSide from '../../components/RightSide/RightSide'
 
 const Home = () => {
   return (
     <div className='home'>
       <ProfileSide />
       <PostSide />
-      <div className='right-side'>RightSide</div>
+      <RightSide />
     </div>
   )
 }
