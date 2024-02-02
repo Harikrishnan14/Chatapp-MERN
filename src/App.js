8a2a855b6326006284d2d7f2658bea4e4ee4a2dc
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-02-02 14:03:54 +0530

    React router setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Auth from './pages/Auth/Auth';
 import Profile from './pages/Profile/Profile';
 import Home from './pages/home/Home';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const user = useSelector((store) => store.authReducer.authData)
+
   return (
     <div className="App">
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
-      <Auth />
+      <Routes>
+        <Route path='/' element={user ? <Navigate to="home" /> : <Navigate to="auth" />} />
+        <Route path='/home' element={user ? <Home /> : <Navigate to="../auth" />} />
+        <Route path='/auth' element={user ? <Navigate to="../home" /> : <Auth />} />
+      </Routes>
     </div>
   );
 }
