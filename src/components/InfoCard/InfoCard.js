0e6ecae5b6e9e6f83ceeda01cdbb60f3819ce597
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-02-10 23:58:54 +0530

    Fetching and displaying data on infoCard

--- src/components/InfoCard/InfoCard.js
+++ src/components/InfoCard/InfoCard.js
@@ -1,16 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './InfoCard.css'
 import { UilPen } from '@iconscout/react-unicons'
 import ProfileModal from '../ProfileModal/ProfileModal'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import * as UserApi from '../../api/UserReq.js'
 
 const InfoCard = () => {
 
     const [openModal, setOpenModal] = useState(false)
+    const [profileUser, setProfileUser] = useState({})
+
+    const dispatch = useDispatch()
+
+    const params = useParams()
+    const profileUserId = params.id
+
+    const { user } = useSelector((state) => state.authReducer.authData)
+
+    useEffect(() => {
+        const fetchProfileUser = async () => {
+            if (profileUserId === user._id) {
+                setProfileUser(user)
+            } else {
+                const profileUser = await UserApi.getUser(profileUserId);
+                setProfileUser(profileUser)
+            }
+        }
+        fetchProfileUser();
+    }, [user]);
 
     return (
         <div className='InfoCard'>
             <div className="info-head">
-                <h4>Your Info</h4>
+                <h4>Profile Info</h4>
+                {user._id === profileUser ? (
+                    <div>
+                        <UilPen width='2rem' height='1.2rem' onClick={() => setOpenModal(true)} />
+                        <ProfileModal openModal={openModal} setOpenModal={setOpenModal} />
+                    </div>
+                ) : ("")}
                 <div>
                     <UilPen width='2rem' height='1.2rem' onClick={() => setOpenModal(true)} />
                     <ProfileModal openModal={openModal} setOpenModal={setOpenModal} />
@@ -20,19 +49,19 @@ const InfoCard = () => {
                 <span>
                     <b>Status </b>
                 </span>
-                <span>in Releationship</span>
+                <span>{profileUser.relationship}</span>
             </div>
             <div className="info">
                 <span>
                     <b>Lives in </b>
                 </span>
-                <span>Kerala</span>
+                <span>{profileUser.livesin}</span>
             </div>
             <div className="info">
                 <span>
                     <b>Works at </b>
                 </span>
-                <span>Google</span>
+                <span>{profileUser.worksAt}</span>
             </div>
             <button className='button logout-btn'>Logout</button>
         </div>
